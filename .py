0d41c55510450e58f48108c5686e92f3382e63a4
Shad47486 <reshadbhuiyan@gmail.com>
2022-08-30 19:42:09 -0400

    new description

--- .py
+++ .py
@@ -10,8 +10,8 @@ import re
 import openpyxl 
 # Importing modules intstalled from pyenv into the python terminal (making sure its the right version 3.8.0) #
 #Section 1: 
-tab1 = pd.read_excel('Data/assigment1.xlsx')  # Name is set to "tab1" for the first sheet, excuting command for pd first tab
-tab2 = pd.read_excel('Data/assigment1.xlsx', sheet_name= 1) # Name is set to "tab2" for second sheet however, sheet_name was required on top of the orginal code to access second sheet
+tab1 = pd.read_excel("C:/Users/Shad/.pyenv/PProjects/hha-data-ingestion/Data/assigment1.xlsx")  # Name is set to "tab1" for the first sheet, excuting command for pd first tab and change file direction to directory of downloaded file
+tab2 = pd.read_excel('C:/Users/Shad/.pyenv/PProjects/hha-data-ingestion/Data/assigment1.xlsx', sheet_name= 1) # Name is set to "tab2" for second sheet however, sheet_name was required on top of the orginal code to access second sheet
 tab1
 tab2
 # Typing "tab1" and "tab2" will now show the different sheets pages respectivly 
@@ -19,4 +19,3 @@ tab2
 data = requests.get('https://data.cms.gov/data-api/v1/dataset/60ccbf1c-d3f5-4354-86a3-465711d81c5a/data') #command allows data to be aquired via api from the website 
 data = data.json() 
 data #allows data to be seen with its categories
-
