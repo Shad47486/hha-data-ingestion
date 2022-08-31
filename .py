b527bca2e22160cc9ff89db14c49e91d44f9b668
Shad47486 <reshadbhuiyan@gmail.com>
2022-08-30 22:42:27 -0400

    Completation of sections 1,2,3

--- .py
+++ .py
@@ -9,6 +9,8 @@ import requests
 import pydub
 import re
 import openpyxl 
+import db_dtypes
+from google.cloud import bigquery
 # Importing modules intstalled from pyenv into the python terminal (making sure its the right version 3.8.0) #
 
 
@@ -22,6 +24,35 @@ tab2
 
 
 #Section 2: trying to get dataset from an api 
+
 data = requests.get('https://data.cms.gov/data-api/v1/dataset/60ccbf1c-d3f5-4354-86a3-465711d81c5a/data') #command allows data to be aquired via api from the website 
 data = data.json() 
 data #allows data to be seen with its categories
+
+#Section 3: 
+
+gcp_project= 'reshad-507' #identifies the project folder
+bq_dataset = 'reshad-507.covid_19_genome_sequence_dataset' #the data set and var name to give as 'bq_dataset'
+bq_dataset1 = 'reshad-507.rxrx19' 
+client = bigquery.Client(project=gcp_project) 
+dataset_ref = client.dataset(bq_dataset, bq_dataset1) #refrenceing the datset that will be used for the bq
+
+def gcp2df(sql) :
+    query = client.query(sql)
+    results = query.result()
+    return results.to_dataframe() #lines 40-43 define the use of sql and defininging the resutlts and where the query would return to I THINK
+
+query = """SELECT * FROM 
+`reshad-507.covid_19_genome_sequence_dataset.metadata` 
+LIMIT 100"""
+query2 = """SELECT * FROM 
+`reshad-507.rxrx19.rxrx19a_metadata` 
+LIMIT 100"""
+#querys of what of all the the columns but only the first 100 from the datasets
+
+bigquery1 = gcp2df(query)
+bigquery1
+
+bigquery2 = gcp2df(query2)
+bigquery2
+#deinfed the name of the dataset to bigquery1 and bigquery 2 and writing only the variable name results in the 
